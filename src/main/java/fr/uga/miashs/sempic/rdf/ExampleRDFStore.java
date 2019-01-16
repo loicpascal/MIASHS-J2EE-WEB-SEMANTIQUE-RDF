04dbffaf4ed3c4b02722e9a75c6b819b840f6e5c
matthieuramage <matthieu.ramage@eolas.fr>
2019-01-16 21:42:35 +0100

    Example

--- src/main/java/fr/uga/miashs/sempic/rdf/ExampleRDFStore.java
+++ src/main/java/fr/uga/miashs/sempic/rdf/ExampleRDFStore.java
@@ -25,9 +25,11 @@ public class ExampleRDFStore {
 
         System.out.println("\n\nOnthologie Depiction :\n");
         
-        //List<Resource> classes = s.listSubClassesOf(SempicOnto.Depiction);
-        //classes.forEach(c -> {System.out.println(c);});
-        
+        List<Resource> classesSelect = s.listDepictionClasses();
+        classesSelect.forEach(c -> {
+            System.out.println(c);
+            System.out.println(c.getProperty(RDFS.label, "fr").getLiteral());
+        });
 
         System.out.println("\n\n Instances Depiction :\n");
         
@@ -36,15 +38,20 @@ public class ExampleRDFStore {
         
 
         System.out.println("\n\n Création photos + annotations :\n");
-        
-        Resource pRes1 = s.createPhoto(1, 1, 1);
-        
         /*
-        s.createAnnotationObject(1, SempicOnto.depicts.getURI(), tika.getURI());
+        Resource pRes1 = s.createPhoto(1, 1, 1);
+        Resource pRes2 = s.createPhoto(2, 1, 1);
+        
         s.createAnnotationData(1, SempicOnto.title.getURI(), "Ma super photo");
         s.createAnnotationObject(1, SempicOnto.takenIn.getURI(), Namespaces.dbr + "Grenoble");
-        s.createAnnotationObject(1, SempicOnto.takenBy.getURI(), newPerson1.getURI());
+        s.createAnnotationData(2, SempicOnto.title.getURI(), "Ma photo selfie");
         */
+        
+        /*
+        //s.createAnnotationObject(1, SempicOnto.depicts.getURI(), tika.getURI());
+
+        //s.createAnnotationObject(1, SempicOnto.takenBy.getURI(), newPerson1.getURI());
+        
         
         List<Resource> resDepict = s.getPhotoDepictions(1);
         resDepict.forEach(r -> {System.out.println(r);});
@@ -55,7 +62,7 @@ public class ExampleRDFStore {
         oP.forEach(r -> {System.out.println(r);});
       
         System.out.println("\n\n listAuthors :\n");
-        Resource pRes2 = s.createPhoto(2, 1, 1);
+        
         //s.createAnnotationObject(2, SempicOnto.takenBy.getURI(), newPerson2.getURI());
         
         //List<Resource> resAuthor = s.listAuthors();
@@ -64,15 +71,19 @@ public class ExampleRDFStore {
         
         System.out.println("\n\n getSelfies :\n");
         //s.createAnnotationObject(2, SempicOnto.depicts.getURI(), newPerson2.getURI());
-        //s.createAnnotationData(2, SempicOnto.title.getURI(), "Ma photo selfie");
+        //
         
         List<Resource> resSelfy = s.getSelfies();
         resSelfy.forEach(r -> {System.out.println(r);});
         
+        */
+        
+
         //s.cleanAllResource(SempicOnto.Depiction.getURI());
-        //s.cleanAllDepicts();
+        
      
         // Test dbpedia
+        /*
         System.out.println("\nDbpedia listPopulatedPlacesConstruct :\n");
         try {
             List<Resource> places = s.listPopulatedPlacesConstruct();
@@ -93,18 +104,18 @@ public class ExampleRDFStore {
                 System.out.println(p.getProperty(FOAF.name, "fr").getLiteral());
                 System.out.println(p);
             }
-            /*
+            
             placesSelect.forEach(p -> {
                 System.out.println(p.getProperty(FOAF.name, "fr").getLiteral());
                 System.out.println(p);
             });
-            */
+            
         } catch (QueryParseException qpe) {
             qpe.printStackTrace();
         } catch(Exception e) {
             e.printStackTrace();
         }
-        
+        */
   
   
         
